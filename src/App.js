4ded8fc8e41de9352b2e44ae12762d3366a56794
Mohit Gupta <mohitguptatech18@gmail.com>
2021-11-11 17:13:16 +0530

    cloning in pc

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AddUser from "./Components/User/AddUser";
+
+const App = () => {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+};
+
+export default App;
