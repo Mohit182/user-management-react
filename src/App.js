70a2471ebd47ff44a7a9f1d64389f4d6041ddadc
Mohit Gupta <mohitguptatech18@gmail.com>
2021-11-13 09:13:33 +0530

    taken input from user and stored in variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import AddUser from "./Components/User/AddUser";
+import UsersList from "./Components/User/UsersList";
 
 const App = () => {
+  const [usersList , setUsersList ] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
+    });
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={[]}/>
     </div>
   );
 };
